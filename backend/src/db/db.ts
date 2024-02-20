c03a89d86810235802605bba61c820e54b53ca3f
MXerFix <maksim2003003@gmail.com>
2024-02-20 10:04:41 +0300

    fix 90485092

--- backend/src/db/db.ts
+++ backend/src/db/db.ts
@@ -17,7 +17,7 @@ const db = async () => {
   if (sequelize) {
     try {
       await sequelize.authenticate()
-      // await sequelize.sync({ alter: true })
+      await sequelize.sync()
       console.log("Connection has been established successfully.")
     } catch (error) {
       console.error("Unable to connect to the database:", error)
